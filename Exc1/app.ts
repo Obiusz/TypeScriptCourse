c4eae09b7275a4b07305186412573e5b6c34dbaf
Obiusz <mateusz@expandingweb.com>
2021-07-14 18:05:08 +0200

    Array-type excercise

--- Exc1/app.ts
+++ Exc1/app.ts
@@ -4,7 +4,15 @@
 // } = {
 const person = {
     name: 'Obisław',
-    age: 33
+    age: 33,
+    hobbies: ['Sports', 'Cooking']
 }
 
+let favoriteActivities: string[]
+favoriteActivities = ['Sports']
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
