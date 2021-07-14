7bfd9e875cb7829f9c95be3854624670177b1a13
Obiusz <mateusz@expandingweb.com>
2021-07-14 18:16:50 +0200

    Tuples-type excercise

--- Exc1/app.ts
+++ Exc1/app.ts
@@ -2,11 +2,23 @@
 //     name: string;
 //     age: number;
 // } = {
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+    //tuple - array złożony wyłącznie z dwóch elementów
+} = {
     name: 'Obisław',
     age: 33,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 }
+
+// person.role.push('admin')
+// person.role[1] = 10
+
+// person.role = [0, 'admin', 'user']
 
 let favoriteActivities: string[]
 favoriteActivities = ['Sports']
