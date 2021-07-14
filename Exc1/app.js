2cc68c8107c46281532628faa8639b039893ae27
Obiusz <mateusz@expandingweb.com>
2021-07-14 18:39:43 +0200

    Enums excercise

--- Exc1/app.js
+++ Exc1/app.js
@@ -2,11 +2,34 @@
 //     name: string;
 //     age: number;
 // } = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+//     //tuple - array złożony wyłącznie z dwóch elementów
+// } = {
+//     name: 'Obisław',
+//     age: 33,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// }
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 5] = "ADMIN";
+    Role[Role["READ_ONLY"] = 6] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 7] = "AUTHOR";
+})(Role || (Role = {}));
+;
+// można przypisać własną wartość enumowi - jakąkolwiek
 var person = {
     name: 'Obisław',
     age: 33,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
 // person.role.push('admin')
 // person.role[1] = 10
@@ -17,4 +40,7 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
 }
--- Exc1/app.ts
+++ Exc1/app.ts
@@ -2,17 +2,31 @@
 //     name: string;
 //     age: number;
 // } = {
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string]
-    //tuple - array złożony wyłącznie z dwóch elementów
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+//     //tuple - array złożony wyłącznie z dwóch elementów
+// } = {
+//     name: 'Obisław',
+//     age: 33,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// }
+
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+
+enum Role { ADMIN = 5, READ_ONLY, AUTHOR };
+// można przypisać własną wartość enumowi - jakąkolwiek
+
+const person = {
     name: 'Obisław',
     age: 33,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 }
 
 // person.role.push('admin')
@@ -27,4 +41,8 @@ console.log(person.name)
 
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase())
+}
+
+if (person.role === Role.AUTHOR) {
+    console.log('is author')
 }
