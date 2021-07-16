51c19bc3956678beb254c4f40a28bd87d3f8d0fd
Obiusz <mateusz@expandingweb.com>
2021-07-16 09:56:03 +0200

    Renamed app.ts to functions.ts and created new app.ts for next lesson - type: unknown

--- Exc1/app.js
+++ Exc1/app.js
@@ -1,20 +1,3 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-// function printResult(num: number): void  {
-//     console.log('Result: ' + num)
-// }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-// printResult(add(5, 12))
-var combineValues;
-//This is how you declare Function types
-combineValues = add;
-// combineValues = printResult
-console.log(combineValues(8, 8));
-// let someValue: undefined
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+var userInput;
+userInput = 5;
+userInput = 'Max';
